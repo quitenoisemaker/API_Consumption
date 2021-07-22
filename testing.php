44c8e3385e2f4191a31930adbef1ba23e0f41595
samson <samsonojugo@gmail.com>
2021-07-22 12:17:45 +0100

    added php CURL

--- testing.php
+++ testing.php
@@ -1,43 +1,5 @@
 <?php
 
-// Method: POST, PUT, GET etc
-// Data: array("param" => "value") ==> index.php?param=value
-
-// function CallAPI($method, $url, $data = false)
-// {
-//     $curl = curl_init();
-
-//     switch ($method)
-//     {
-//         case "POST":
-//             curl_setopt($curl, CURLOPT_POST, 1);
-
-//             if ($data)
-//                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-//             break;
-//         case "PUT":
-//             curl_setopt($curl, CURLOPT_PUT, 1);
-//             break;
-//         default:
-//             if ($data)
-//                 $url = sprintf("%s?%s", $url, http_build_query($data));
-//     }
-
-//     // Optional Authentication:
-//     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-//     curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-//     curl_setopt($curl, CURLOPT_URL, $url);
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-//     $result = curl_exec($curl);
-
-//     curl_close($curl);
-
-//     return $result;
-// }
-
-// echo CallAPI("GET", "https://randomuser.me/api");
 
 $url = 'https://randomuser.me/api';
 $ch = curl_init($url);
@@ -47,9 +9,6 @@ $response_json = curl_exec($ch);
 curl_close($ch);
 $response=json_decode($response_json, true);
  
-
-//  foreach($response as $key => $value) {
-//   print_r($value) . "<br>";
-// }
+print_r($response['results'][0]['gender']);
 
 ?>
